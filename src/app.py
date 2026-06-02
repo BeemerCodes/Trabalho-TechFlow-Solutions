6ba3d49c45d322bc177c8c1508196c12767ee8d9
Pedro Ruivo <beemercodes@MacBook-Pro.local>
2026-06-02 13:24:10 -0300

    feat: cria modelagem da entidade Task e conexao SQLite

---
+++ src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    status = db.Column(db.String(30), default='A Fazer')
+
+with app.app_context():
+    db.create_all()
